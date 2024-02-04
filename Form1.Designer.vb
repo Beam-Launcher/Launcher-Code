dcd68e5951a88bda673a38cfbc0b26589f3045a0
Willfa10 <101812021+Willfa10@users.noreply.github.com>
2024-02-04 15:57:45 +0000

    Add files via upload

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -29,18 +29,21 @@ Partial Class Form1
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.Button7 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
         Me.Button10 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.BackColor = System.Drawing.Color.Linen
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(-63, -55)
         Me.PictureBox1.Name = "PictureBox1"
@@ -97,19 +100,6 @@ Partial Class Form1
         Me.Button7.TabIndex = 7
         Me.Button7.UseVisualStyleBackColor = True
         '
-        'TextBox1
-        '
-        Me.TextBox1.BackColor = System.Drawing.Color.Linen
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Default
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!)
-        Me.TextBox1.ForeColor = System.Drawing.Color.Black
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(587, 31)
-        Me.TextBox1.TabIndex = 9
-        Me.TextBox1.Text = "BeamNG.Drive Launcher" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
         'Button8
         '
         Me.Button8.Image = CType(resources.GetObject("Button8.Image"), System.Drawing.Image)
@@ -156,17 +146,60 @@ Partial Class Form1
         Me.Button4.TabIndex = 13
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.Transparent
+        Me.Button11.Location = New System.Drawing.Point(12, 345)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(237, 68)
+        Me.Button11.TabIndex = 14
+        Me.Button11.Text = "Open Beam-Launcher's Website"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.Transparent
+        Me.Button12.Location = New System.Drawing.Point(12, 310)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(316, 29)
+        Me.Button12.TabIndex = 15
+        Me.Button12.Text = "Contact via email"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.Image = CType(resources.GetObject("Button13.Image"), System.Drawing.Image)
+        Me.Button13.Location = New System.Drawing.Point(255, 345)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(73, 68)
+        Me.Button13.TabIndex = 16
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 37)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Beam-launcher v0.3"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Linen
         Me.ClientSize = New System.Drawing.Size(578, 416)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button10)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button6)
@@ -189,10 +222,13 @@ Partial Class Form1
     Friend WithEvents Button3 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents Button7 As Button
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button8 As Button
     Friend WithEvents Button5 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Button10 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Label1 As Label
 End Class
