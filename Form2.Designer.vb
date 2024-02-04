7d0409c62d5d2e9bddd1596dc4a3d8fa0ec666d1
Willfa10 <101812021+Willfa10@users.noreply.github.com>
2024-02-04 15:54:38 +0000

    Add files via upload

---
+++ Form2.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(12, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Themes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(177, 19)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(116, 62)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "Light theme (default)"
+        Me.Button7.UseVisualStyleBackColor = True
+        Me.Button7.Visible = False
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(177, 87)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(116, 64)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "Dark theme"
+        Me.Button8.UseVisualStyleBackColor = True
+        Me.Button8.Visible = False
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(177, 157)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(116, 63)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "Dark blue theme"
+        Me.Button9.UseVisualStyleBackColor = True
+        Me.Button9.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(12, 66)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 41)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Text colour"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Location = New System.Drawing.Point(12, 160)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 41)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Background colour"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(177, 157)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(116, 63)
+        Me.Button11.TabIndex = 16
+        Me.Button11.Text = "Black"
+        Me.Button11.UseVisualStyleBackColor = True
+        Me.Button11.Visible = False
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(177, 87)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(116, 63)
+        Me.Button15.TabIndex = 21
+        Me.Button15.Text = "Grey"
+        Me.Button15.UseVisualStyleBackColor = True
+        Me.Button15.Visible = False
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(177, 18)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(116, 63)
+        Me.Button10.TabIndex = 23
+        Me.Button10.Text = "White"
+        Me.Button10.UseVisualStyleBackColor = True
+        Me.Button10.Visible = False
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(177, 87)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(116, 63)
+        Me.Button12.TabIndex = 24
+        Me.Button12.Text = "Dark Blue"
+        Me.Button12.UseVisualStyleBackColor = True
+        Me.Button12.Visible = False
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(177, 157)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(116, 63)
+        Me.Button14.TabIndex = 25
+        Me.Button14.Text = "Linen (light orange)"
+        Me.Button14.UseVisualStyleBackColor = True
+        Me.Button14.Visible = False
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(177, 18)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(116, 63)
+        Me.Button16.TabIndex = 26
+        Me.Button16.Text = "Dark grey"
+        Me.Button16.UseVisualStyleBackColor = True
+        Me.Button16.Visible = False
+        '
+        'Button4
+        '
+        Me.Button4.CausesValidation = False
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Location = New System.Drawing.Point(12, 113)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(106, 41)
+        Me.Button4.TabIndex = 27
+        Me.Button4.Text = "Language"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(177, 19)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(116, 63)
+        Me.Button5.TabIndex = 28
+        Me.Button5.Text = "English (GB)"
+        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(177, 88)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(116, 63)
+        Me.Button6.TabIndex = 29
+        Me.Button6.Text = "English (US)"
+        Me.Button6.UseVisualStyleBackColor = True
+        Me.Button6.Visible = False
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(177, 157)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(116, 63)
+        Me.Button13.TabIndex = 30
+        Me.Button13.Text = "Español"
+        Me.Button13.UseVisualStyleBackColor = True
+        Me.Button13.Visible = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
+        Me.ClientSize = New System.Drawing.Size(497, 393)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form2"
+        Me.Text = "Beam-Launcher UI preferences"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button13 As Button
+End Class
---
+++ Form2.vb
@@ -0,0 +1,200 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form2
+    Private Form1Instance As Form1
+
+    Public Sub New(form1Instance As Form1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        InitializeComponent()
+        Me.Form1Instance = form1Instance ' Set the Form1Instance reference
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button7.Visible = False
+        Button8.Visible = False
+        Button9.Visible = False
+        Button15.Visible = True
+        Button11.Visible = True
+        Button10.Visible = True
+        Button5.Visible = False
+        Button12.Visible = False
+        Button13.Visible = False
+        Button6.Visible = False
+        Button14.Visible = False
+        Button16.Visible = False
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim colorToSet As Color = Color.Linen ' Change the color as needed
+        Form1Instance.ChangeTextColor(Color.Black)
+
+        ChangeAllTextColors(Color.Black)
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+        Form1Instance.ChangeButtonColors(Color.White, Color.Black)
+        Form1Instance.ChangeTextColor(Color.Black)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim colorToSet As Color = Color.FromArgb(32, 32, 32) ' Change the color as needed
+        Form1Instance.ChangeTextColor(Color.White)
+        ChangeAllTextColors(Color.Black)
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+        Form1Instance.ChangeButtonColors(Color.Black, Color.White)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim colorToSet As Color = Color.FromArgb(32, 32, 54) ' Change the color as needed
+        Form1Instance.ChangeTextColor(Color.White)
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+        Form1Instance.ChangeButtonColors(Color.Black, Color.White)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button15.Visible = False
+        Button11.Visible = False
+        Button10.Visible = False
+        Button5.Visible = False
+        Button13.Visible = False
+        Button6.Visible = False
+        Button7.Visible = True
+        Button8.Visible = True
+        Button9.Visible = True
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+        Button11.Visible = False
+        Button5.Visible = False
+        Button10.Visible = False
+        Button6.Visible = False
+        Button15.Visible = False
+        Button13.Visible = False
+    End Sub
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs)
+        Button10.Visible = False
+        Button11.Visible = False
+        Button5.Visible = False
+        Button15.Visible = False
+        Button6.Visible = False
+        Button13.Visible = False
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs)
+        Button10.Visible = True
+        Button11.Visible = True
+        Button15.Visible = True
+        Button10.Visible = True
+        Button11.Visible = True
+        Button15.Visible = True
+        Button5.Visible = False
+        Button13.Visible = False
+        Button6.Visible = False
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Form1Instance.ChangeTextColor(Color.Black)
+    End Sub
+
+    Private Sub ChangeAllTextColors(color As Color)
+        ' Iterate through all controls in the form
+        For Each control As Control In Me.Controls
+            control.ForeColor = color
+        Next
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Form1Instance.ChangeTextColor(Color.White)
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Form1Instance.ChangeTextColor(Color.Gray)
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim colorToSet As Color = Color.FromArgb(32, 32, 54)
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Dim colorToSet As Color = Color.Linen
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Dim colorToSet As Color = Color.FromArgb(32, 32, 32)
+        Form1Instance.ChangeBackgroundColor(colorToSet)
+        Me.BackColor = colorToSet
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button12.Visible = True
+        Button14.Visible = True
+        Button16.Visible = True
+        Button15.Visible = False
+        Button7.Visible = False
+        Button8.Visible = False
+        Button9.Visible = False
+        Button10.Visible = False
+        Button11.Visible = False
+        Button15.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+        Button13.Visible = False
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        Button12.Visible = False
+        Button14.Visible = False
+        Button16.Visible = False
+        Button15.Visible = False
+        Button7.Visible = False
+        Button8.Visible = False
+        Button9.Visible = False
+        Button10.Visible = False
+        Button11.Visible = False
+        Button15.Visible = False
+        Button5.Visible = True
+        Button6.Visible = True
+        Button13.Visible = True
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Button2.Text = Button2.Text.Replace("color", "colour")
+        Form1Instance.Button1.Text = Form1Instance.Button1.Text.Replace("Lanzar", "Launch")
+        Form1Instance.Button2.Text = Form1Instance.Button2.Text.Replace("Abrir Carpeta de Mods", "Open Mods Folder")
+        Form1Instance.Button3.Text = Form1Instance.Button3.Text.Replace("Administrar carpeta de usuario", "Manage User Folder")
+        Form1Instance.Button10.Text = Form1Instance.Button10.Text.Replace("Ver las últimas noticias", "View Latest News")
+    End Sub
+
+    Private Sub Button6_Click_1(sender As Object, e As EventArgs) Handles Button6.Click
+        Button2.Text = Button2.Text.Replace("colour", "color")
+        Form1Instance.Button1.Text = Form1Instance.Button1.Text.Replace("Lanzar", "Launch")
+        Form1Instance.Button2.Text = Form1Instance.Button2.Text.Replace("Abrir Carpeta de Mods", "Open Mods Folder")
+        Form1Instance.Button3.Text = Form1Instance.Button3.Text.Replace("Administrar carpeta de usuario", "Manage User Folder")
+        Form1Instance.Button10.Text = Form1Instance.Button10.Text.Replace("Ver las últimas noticias", "View Latest News")
+
+    End Sub
+
+    Private Sub Button13_Click_1(sender As Object, e As EventArgs) Handles Button13.Click
+        Form1Instance.Button1.Text = Form1Instance.Button1.Text.Replace("Launch", "Lanzar")
+        Form1Instance.Button2.Text = Form1Instance.Button2.Text.Replace("Open Mods Folder", "Abrir Carpeta de Mods")
+        Form1Instance.Button3.Text = Form1Instance.Button3.Text.Replace("Manage User Folder", "Administrar carpeta de usuario")
+        Form1Instance.Button10.Text = Form1Instance.Button10.Text.Replace("View Latest News", "Ver las últimas noticias")
+    End Sub
+End Class
+
